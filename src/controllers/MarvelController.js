9699eecff9e84ee2262dde29e0dcd57ee56f5eff
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-04-21 20:51:59 -0300

    Add fetchCharacters in the controller methods

--- src/controllers/MarvelController.js
+++ src/controllers/MarvelController.js
@@ -1,6 +1,10 @@
 import MarvelRepository from "../repositories/MarvelRepository";
 
 export default class MarvelController {
+  async fetchCharacters() {
+    return await MarvelRepository.fetchCharacters();
+  }
+
   async fetchComics() {
     return await MarvelRepository.fetchComics();
   }

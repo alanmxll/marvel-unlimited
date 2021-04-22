687bc9f8e136ddc74e7cec93ff01e83a8b9b6f0a
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-04-21 21:53:59 -0300

    Add Card component

---
+++ src/components/Card/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  background-color: #555;
+  border: 1px solid white;
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px #fff;
+  margin-bottom: 8px;
+`;
+
+export const CardLabel = styled.Text`
+  align-self: center;
+  font-size: 20px;
+  color: #fff;
+`;
+
+export const HeroImage = styled.Image`
+  height: 175px;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+`;

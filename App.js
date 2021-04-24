0364d7aec1d0987910153b4afec41e60f57293ab
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-04-24 10:22:47 -0300

    Configure Navigation container and stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import React, { Fragment } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
 import { StatusBar } from "react-native";
+import Comics from "./src/components/Comics";
 import Home from "./src/components/Home";
+import Title from "./src/components/Title";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Fragment>
-      <Home />
+    <NavigationContainer>
       <StatusBar />
-    </Fragment>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTitle: Title,
+          headerStyle: {
+            backgroundColor: "#333",
+            elevation: 0,
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Comics" component={Comics} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
